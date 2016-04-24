1d2205bf17ef16a0843e70ea75f69bbe33621554
ricikie <hermawan.rick@gmail.com>
2016-04-24 17:17:26 +0700

    updatean
    1.video play
    2.scroll button
    3.contact
    4.footer
    5.easing
    4.footer
    5.another fix :v

--- js/script.js
+++ js/script.js
@@ -5,8 +5,64 @@ $(document).ready(function(){
 		target:'.navbar',
 		offset:160
 	});
+
+animationHover('#video_button','tada');
+function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+        function() {
+            element.addClass('animated ' + animation);        
+        },
+        function(){
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 2000);         
+        });
+}
+animationHover('#headerarrow','swing');
+function scrollHover(element, animation){
+    element = $(element);
+    element.hover(
+        function() {
+            element.addClass(animation);        
+        },
+        function(){
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass(animation);
+            }, 2000);         
+        });
+}
+
+
 });
 
+
+// SCROLLUP BOTTON
+
+$('.scrollup').click(function(){
+$("html, body").animate({scrollTop:0},2000);
+return false;
+});
+
+// SCROLLDOWN-MOUSE
+
+$('.scrolldown a').bind('click', function(){
+	$('html, body').stop().animate({
+		scrollTop:$($(this).attr('href')).offset().top - 100
+	},900, 'easeInOutExpo');
+	event.preventDefault();
+});
+
+// NAVBAR
+
+$('.nav a').bind('click', function(){
+	$('html, body').stop().animate({
+		scrollTop:$($(this).attr('href')).offset().top - 50
+	},900, 'easeInOutExpo');
+	event.preventDefault();
+});
 
 // ====Navbar fixed=====
 /*
